f6e189e529c99ba51e90ec63738e676a511d8149
6826672@gmail.com <6826672@gmail.com>
2023-06-09 02:11:04 +0800

    first commit

---
+++ learning_logs/urls.py
@@ -0,0 +1,27 @@
+''' 定义learnging_logs的URL模式'''
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # 定义主页
+    path('', views.index, name='index'),
+
+    # 显示所有的主题页面
+    path('topics/', views.topics, name='topics'),
+
+    # 特定主题的详细页面
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    # 添加新主题的页面
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # 添加新条目的页面
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # 编辑条目页面
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+
+]
+
